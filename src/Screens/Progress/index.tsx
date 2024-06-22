d98339a5bb219b391079adbd138791df3c6ec28a
Abdelaziz <amabdelaziz@ejada.com>
2024-06-22 16:02:28 +0300

    finish date picker component

--- src/Screens/Progress/index.tsx
+++ src/Screens/Progress/index.tsx
@@ -1,11 +1,24 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+import React, {useState} from 'react';
 import {colors} from '@assets/colors';
+import styles from './styles';
+import DatePicker from 'react-native-date-picker';
 
 const Progress = () => {
+  const [date, setDate] = useState(new Date());
   return (
-    <View style={{backgroundColor: colors.white, flex: 1}}>
-      <Text>Progress</Text>
+    <View style={styles.mainContainer}>
+      <Text>show date</Text>
+
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <DatePicker
+          date={date}
+          onDateChange={setDate}
+          mode="time"
+          dividerColor={colors.lilacPetals}
+          style={{backgroundColor: colors.lilacPetals, borderRadius: 20}}
+        />
+      </View>
     </View>
   );
 };
---
+++ src/Screens/Progress/styles.ts
@@ -0,0 +1,10 @@
+import {colors} from '@assets/colors';
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  mainContainer: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+});
